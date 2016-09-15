063d247e98aa7700e71c438b52bb90e153d648e7
Michael Harrison <harrim91@hotmail.co.uk>
2016-09-15 17:52:10 +0100

    removes comments, moves terminal testing file, refactors to extract strip_punctuation method

--- lib/string_censor.rb
+++ lib/string_censor.rb
@@ -17,19 +17,21 @@ class StringCensor
 	def censor string
 		words = string.split
 		words.each do |word|
-			# p word
-			# p allowed? word.gsub(/\W+/, '')
-			word.gsub! /[AEIOUaeiou]/, '-' unless allowed? word.gsub(/\W+/, '')
+			word.gsub! /[AEIOUaeiou]/, '-' unless allowed?(strip_punctuation word)
 		end
 		words.join ' '
 	end
 
 	private
 	def allowed? word
-		@exceptions.include?(word.downcase) ||
 		@banned_words.none? do |banned_word| 
 			Regexp.new(banned_word).match word.downcase
-		end
+		end ||
+		@exceptions.include?(word.downcase)
+	end
+
+	def strip_punctuation word
+		word.gsub(/\W/, '')
 	end
 
 end
